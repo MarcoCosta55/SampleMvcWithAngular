43056bf822208eae06c52cf1c28403bd9c35c95e
MarcoLaptop <51866687+MarcoCosta55@users.noreply.github.com>
2020-06-24 11:51:06 -0400

    Did Initial Migration

--- SampleMvcWithAngular/Data/ApplicationDbContext.cs
+++ SampleMvcWithAngular/Data/ApplicationDbContext.cs
@@ -18,10 +18,10 @@ namespace SampleMvcWithAngular.Data
         {
         }
 
-        // Add a DbSet for each entity type that you want to include in your model. For more information 
-        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
         public static ApplicationDbContext Create() => new ApplicationDbContext();
 
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
         public virtual DbSet<JoggingRecord> JoggingRecords { get; set; }
     }
 }
